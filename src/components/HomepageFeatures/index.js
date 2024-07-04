c72ff53627e9e400289a5db1e25276ef53b55364
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-04 18:33:50 -0400

    changing the front page content

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: ' mostafas 1st header',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+       i am trying to change to this website to be usbale for other purposes 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'mostafas 2nd header',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+       this site is mainly used for documentation
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'mostafas 3rd header',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        i really ran out of ideas please change 
       </>
     ),
   },
