e31e075c2328d0a25ca2580db4b6415323abb43c
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2024-07-04 19:53:49 -0400

    changing the website icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,12 +64,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/website-undraw.png',
       navbar: {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/website-undraw.svg',
         },
         items: [
           {
